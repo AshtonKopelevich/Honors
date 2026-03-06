97ffc1676359736ba360cf573f5437696428786f
Ashton Kopelevich <AshtonKopelevichWork@gmail.com>
2026-03-06 00:57:49 -0600

    Finished the final code in csvsorter_finished.cpp. The csvsorter.cpp is the bug ridden code.

--- csvsorter.cpp
+++ csvsorter.cpp
@@ -1,13 +1,16 @@
-#include <vector>
+/*#include <vector>
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <limits>
 
 using std::vector;
 using std::string;
 using std::cout;
 using std::cin;
-//This program is simple and find the minimum and maximum numbers from a csv file, and prints it. WHAT COULD GO WRONG?!!
+//This program is simple and reads a csv file before sorting the data and printing the min and max. 
+//WHAT COULD GO WRONG?!!
+
 //cast string to int safely
 int string_to_int(string str)
 {
@@ -42,17 +45,17 @@ vector<int> process_csv(string file_name)
             
             //split a csv line into separate values
             string str_num = ""; //each unique number, but gotten raw from the string
-            for(int i = 0; i < line.size(); i++)
+            for(size_t i = 0; i < line.size(); i++)
             {
                 if(line.at(i) == ',')
                 {
                     int num = string_to_int(str_num);
                     values.push_back(num);
-                    str_num = "";
                     continue;
                 }
                 str_num += line.at(i);
             }
+
         }
     }
     else
@@ -63,17 +66,17 @@ vector<int> process_csv(string file_name)
     return(values);
 }
 
-
+//This algorithm sorts numbers using insertion sort
 vector<int> sort_numbers(vector<int> numbers){
     vector<int> numbers_sorted{};
     bool is_inserted = false; // checks if the number has already been inserted into the sorted vector
 
-    for(unsigned int i = 0; i< numbers.size(); i++){// iterrate through list one
+    for(unsigned int i = 0; i< numbers.size(); i++){// iterate through list one
         is_inserted = false;
 
         for(unsigned int j = 0; j< numbers_sorted.size(); j++){// iterate through list two per value in list one
             if(numbers.at(i) < numbers_sorted.at(j)){
-                numbers_sorted.insert(numbers_sorted.begin()+j, numbers.at(i));
+                numbers_sorted.insert(numbers_sorted.begin(), numbers.at(i));
                 is_inserted = true;
                 break;//breaks out when found
             }
@@ -86,11 +89,18 @@ vector<int> sort_numbers(vector<int> numbers){
     return numbers_sorted;
 } 
 
-void print_min_and_max(vector<int> numbers)
+//This prints the minimum, maximimum, as well as the sorted form of the vector
+void print_vector(vector<int> numbers)
 {
     numbers = sort_numbers(numbers);
     cout << "Minimum: " << numbers.at(0) << std::endl;
-    cout << "Maximum: " << numbers.at(numbers.size()-1) << std::endl;
+    cout << "Maximum: " << numbers.at(numbers.size()) << std::endl;
+    cout << "Sorted vector: [";
+    for(size_t i = 0; i < numbers.size()-1; i++)
+    {
+        cout << numbers.at(i) << ",";
+    }
+    cout << numbers.at(numbers.size()-1) << "]" << std::endl;
 }
 
 int main()
@@ -104,6 +114,6 @@ int main()
         cout << "An error occured, exiting" << std::endl;
         return 1;
     }
-    print_min_and_max(nums);
+    print_vector(nums);
     return 0;
-}
+}*/
